fc0572fdcbe471f6880ad638086d26107c4914a6
Moe Amgad <m.amged@hotmail.com>
2024-01-09 15:01:47 +0400

    add ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.12;
+
+contract Ownable {
+    address public owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        require(newOwner != address(0), "Invalid new owner address");
+        emit OwnershipTransferred(owner, newOwner);
+        owner = newOwner;
+    }
+}
